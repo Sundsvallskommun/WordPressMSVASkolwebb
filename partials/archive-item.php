b2b978f140e850bd467ec6c81fe305eb66baa62a
Patrik Jansson <patrik.jansson@cybercom.com>
2018-05-24 15:59:39 +0200

    own template for tag archive

---
+++ partials/archive-item.php
@@ -0,0 +1,18 @@
+<div class="archive-post">
+	<a href="<?php the_permalink(); ?>">
+
+
+		<div class="archive-post__content">
+
+			<h2 class="archive-post__title">
+				<?php the_title(); ?>
+			</h2>
+
+			<div class="archive-post__date text-muted">
+				<?php echo get_the_date(); ?>
+			</div>
+
+		</div>
+
+	</a>
+</div>
